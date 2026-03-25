3ec830828c2113df1369a8fa72f7847a3a1985a7
beshirr <youssefahmedbeshir@gmail.com>
2026-03-25 20:30:10 +0200

    changing my ways

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 
-
-#define HEAP_CAPACITY 640000
-
-char heap[HEAP_CAPACITY] = {0};
-size_t heap_size = 0;
-
 void* alloc(size_t size) {
 
 }
 
-void free_mem(void* ptr) {
+void free(void* p) {
+
+}
+
+int main() {
 
 }
